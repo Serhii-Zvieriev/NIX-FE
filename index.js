632dffc03fc826e7acf9bb17d2e19619d28e89ec
Serhii-Zvieriev <zacjok@gmail.com>
2023-01-19 23:04:57 +0200

    complete tast4

---
+++ index.js
@@ -0,0 +1,141 @@
+// ========== TASK 1 ==========
+const div = document.querySelector("div");
+const ul = document.querySelector("ul");
+const secondLi = ul.childNodes[3];
+// console.log(ul.children[0].previousSibling);
+// ========== END TASK 1 ============================================================
+
+// ========== TASK 2 ==========
+/*Якщо elem – довільний вузол DOM-елемента.
+• Правда, що elem.lastChild.nextSibling завжди дорівнює null ? всегда -
+Свойство Node.nextSibling используется только для чтения и возвращает узел, 
+непосредственно следующий за данным узлом в списке childNodes его родительского элемента, 
+или null если данный узел последний в этом списке.
+
+• Правда, що elem.children[0].previousSibling завжди дорівнює null? - нет, если после тега родителя
+стоит перенос строки (что чаще всего бывает), вернет #text*/
+// ========== END TASK 2 ============================================================
+
+// ========== TASK 3 ==========
+//Пришлось немного запариться чтобы создать таблицу через цикл в JS, но было интересно
+const divTask3 = document.querySelector(".task-3");
+function createTable() {
+  let table = document.createElement("table");
+
+  for (let j = 0; j < 5; j++) {
+    let tr = table.appendChild(document.createElement("tr"));
+    for (let i = 0; i < 5; i++) {
+      let td = document.createElement("td");
+      tr.appendChild(td);
+      td.textContent = `${j + 1}.${i + 1}`;
+      td.style.border = "1px solid purple";
+      if (i === j) {
+        td.style.background = "red";
+      }
+    }
+  }
+
+  divTask3.append(table);
+}
+
+createTable();
+// ========== END TASK 3 ============================================================
+
+// ========== TASK 4 (в задании второе третье) ==========
+//• Таблицю з id="age-table".
+const tableTask4 = document.querySelector("#age-table");
+// console.log(tableTask4);
+//• Всі елементи label всередині цієї таблиці (їх три).
+const childrenTable = tableTask4.querySelectorAll("label");
+// console.log(childrenTable);
+//• Перший td у цій таблиці (зі словом "Age ").
+const firstTd = tableTask4.querySelector("td"); // так как нужно найти первую ячеку, доп параметры можно не указывать
+// console.log(firstTd);
+//• Форму form з ім'ям name = "search".
+const formSearch = document.querySelector("form[name=search]");
+// console.log(formSearch);
+//• Перший input у цій формі.
+const firstInput = formSearch.children[0].children[0]; //firstElementChild
+// console.log(firstInput);
+//• Останній input у цій формі.
+const lastInput = formSearch.children[1]; //lastElementChild
+// console.log(lastInput);
+// ========== END TASK 4 ============================================================
+
+// ========== TASK 4 ==========
+//1. Який у ньому текст (без піддерева)?
+const ulTask4 = document.querySelector(".tast-4");
+
+function showTextChildren() {
+  for (const child of ulTask4.children) {
+    console.log(child.textContent);
+  }
+}
+// showTextChildren();
+//2. Яка кількість нащадків – всіх вкладених (включаючи глибоко вкладені)?
+
+function getAmountChildren(par) {
+  let count = 0;
+
+  if (par.children.length) {
+    for (let i = 0; i < par.children.length; i += 1) {
+      count += 1;
+      console.log(par.children[i]);
+      getAmountChildren(par.children[i]);
+    }
+    // return function () {
+    //   return (count += 1);
+    // };
+    return count;
+  } else {
+    return count;
+  }
+}
+
+//return function () {
+//return (count += 1);
+//};
+console.log(getAmountChildren(ulTask4));
+// console.log(getAmountChildren(ulTask4.children[0]));
+// console.log(getAmountChildren(ulTask4.children[1]));
+// ========== END TASK 4 ============================================================
+
+// ========== TASK 5 ==========
+
+// ========== END TASK 5 ============================================================
+
+// ========== TASK 6 ==========
+
+// ========== END TASK 6 ============================================================
+
+// ========== TASK 7 ==========
+
+// ========== END TASK 7 ============================================================
+
+// ========== TASK 8 ==========
+
+// ========== END TASK 8 ============================================================
+
+// ========== TASK 9 ==========
+
+// ========== END TASK 9 ============================================================
+
+// ========== TASK 10 ==========
+
+// ========== END TASK 10 ============================================================
+
+// ========== TASK 11 ==========
+
+// ========== END TASK 11 ============================================================
+
+// ========== TASK 12 ==========
+
+// ========== END TASK 12 ============================================================
+
+// ========== TASK 13 ==========
+
+// ========== END TASK 13 ============================================================
+
+// ========== TASK 14 ==========
+
+// ========== END TASK 14 ============================================================
